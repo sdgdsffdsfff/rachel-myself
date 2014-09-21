5007eebf7f15e0057ac0b1337f74e6deea3ab9f2
霍本林 <benlinhuo@anjuke.com>
2014-09-21 16:31:21 +0800

    customEvent

--- lib-myself/base.js
+++ lib-myself/base.js
@@ -26,16 +26,19 @@
 	*/
 	_lib.inherit = function(self, parent, args) {
 		//让父构造函数在子构造函数的上下文环境中执行，则会让child拥有parent的所有函数和方法
+        //继承属性
 		parent.apply(self, args);
 		//同时让parent的prototype上内容也赋值给child
+        //继承方法
 		$.extend(self.constructor.prototype, parent.prototype);
 	}
 
 	/*
-	* 自定义事件
+	* 自定义事件（订阅/发布模式）
 	* 原理：实际上就是一个事件队列，on的时候先放入队列，trigger的时候从队列中拿出来执行(每个事件的)
 	*/
-	;(function(L) {
+    _lib.customEvent = function() {}
+	;(function(E) {
 
 		var list = {}, uid = -1;
 
@@ -46,7 +49,7 @@
 		* @return [int] key 用于后面指定解绑某个事件的某个callback
 		* 说明：每一个eventname对应的是callback数组
 		*/
-		L.prototype.on = function(eventnames, callback) {
+		E.prototype.on = function(eventnames, callback) {
 			var parts = eventnames.split(' '), i, key, keys = [];
 			for (i = 0; i < parts.length; i++) {
 				!list[parts[i]] && (list[parts[i]] = []);
@@ -65,11 +68,12 @@
 		* @param [String] eventname 要触发的事件名
 		* @param [Array] args 事件callback执行的参数
 		*/
-		L.prototype.trigger = function(eventname, args) {
+		E.prototype.trigger = function(eventname, args) {
 			if (!list[eventname]) return;
 			var i, en;
+            args = $.isArray(args) ? args : [args];
 			for (i = 0; i < list[eventname].length; i++) {
-				en = list[eventname][i];
+				en = list[eventname][i].fn;
 				en.apply(this, args);
 			}
 		}
@@ -79,14 +83,14 @@
 		* @param [String] eventname 事件名称
 		* @param [Array] keys on绑定事件返回值的数组
 		*/
-		L.prototype.off = function(eventname, keys) {
+		E.prototype.off = function(eventname, keys) {
 			if (!list[eventname]) return;
 			var i, event;
 			!!keys && !$.isArray(keys) && (keys = [keys]);
 			//针对eventname进行的循环
 			for (i = 0; i < list[eventname].length; i++) {
 				event = list[eventname][i];
-				$.inArray(event.key, keys) && (list[eventname][i] = undefined);
+                ($.inArray(event.key, keys) > -1) && (list[eventname][i] = undefined);
 			}
 		}
 
@@ -95,7 +99,7 @@
 		* @param [String] eventnames 绑定的事件名字，可以同时是多个，以空格隔开
 		* @param [fn] callback 回调函数
 		*/
-		L.prototype.once = function(eventname, callback) {
+		E.prototype.once = function(eventname, callback) {
 			var self = this;
 			var keys = self.on(eventname, function() {
 				callback.apply(this, arguments);
@@ -103,7 +107,7 @@
 			});
 		}
 
-	})(_lib);
+	})(_lib.customEvent);
 
 	window.lib = _lib;
 console.log('llll')
---
+++ lib-myself/modules/switch.js
@@ -0,0 +1,7 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: benlinhuo
+ * Date: 14-9-21
+ * Time: 下午4:30
+ * To change this template use File | Settings | File Templates.
+ */
