488e430f4260f15070e89c58d52243d775bf437f
benlinhuo <benlinhuo@gmail.com>
2014-11-13 17:02:10 +0800

    node framework

--- demo/nodeframework/lib/App.js
+++ demo/nodeframework/lib/App.js
@@ -1,1 +1,43 @@
-dfsdf
+/*
+应用程序类，可以通过new App()来创建一个应用程序
+*/
+var http = require('http');
+module.exports = function App() {
+	//中间件的有序列表
+	this._midWareList = [];
+
+	this._server = http.createServer(handler);
+
+	function handler(req, res) {
+		if (!!this._midWareList.length) {
+			return;
+		}
+		var midWareIndex = 0;
+     
+     	//第一次执行该函数时，会默认执行第一个中间件（即开个头）
+ 		execMidWare();
+
+ 		//执行该函数时（会在中间件中执行），会自动执行下一个middleware。
+		function next() {
+			midWareIndex++;
+			execMidWare();
+		}
+
+		//执行中间件
+		function execMidWare() {
+			var midWare = this._midWareList[midWareIndex];
+			midWare && midWare(req, res, next);
+		}
+	}
+
+}
+
+//push一个新的中间件到middleList数组中，以备执行
+App.prototype.use = function(midWare) {
+	this._midWareList.push(midWare);
+}
+
+//将原生的http模块的listen挂载到了App上，所以它的参数同原生
+App.prototype.listen = function() {
+	this._server.listen.apply(this._server, arguments);
+}
--- demo/nodeframework/lib/static.js
+++ demo/nodeframework/lib/static.js
@@ -1,1 +1,29 @@
-dfsdf
+/*
+这是个静态资源服务器，可以根据url中指定的pathname来读取对应文件的内容并返回
+*/
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+module.exports = function static(parPath) {
+    //这个插件无需next
+	return function(req, res, next) {
+		var staticPath = parPath + url2Path(req.url);
+		fs.readFile(staticPath, callback);
+		function callback(err, data) {
+			if (err) {
+				res.statusCode = 404;
+			} else {
+				res.write(data);
+			}
+			res.end();
+		}
+	}
+
+	function url2Path(reqUrl) {
+		var pathname = url.parse(reqUrl).pathname;
+		return pathname == '/' ? 'index.html' : pathname;
+	}
+}
+
+
